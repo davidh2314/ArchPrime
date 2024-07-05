19a58835108d02139699857f1cdbfe97819adb5a
davidh2314 <114437581+davidh2314@users.noreply.github.com>
2024-07-05 11:30:59 -0700

    ArchPrime.sh

--- ArchPrime.sh
+++ ArchPrime.sh
@@ -9,7 +9,7 @@ clear
 # Cosmetics (colours for text).
 BOLD='\e[1m'
 BRED='\e[91m'
-BBLUE='\e[34m'  
+BBLUE='\e[34m'
 BGREEN='\e[92m'
 BYELLOW='\e[93m'
 RESET='\e[0m'
@@ -21,7 +21,7 @@ info_print () {
 
 # Pretty print for input (function).
 input_print () {
-    echo -ne "${BOLD}${BYELLOW}[ ${BGREEN}•${BYELLOW} ] $1${RESET}"
+    echo -ne "${BOLD}${BYELLOW}[ ${GREEN}•${BYELLOW} ] $1${RESET}"
 }
 
 # Alert user of bad input (function).
@@ -142,9 +142,6 @@ desktop_selector () {
     pacstrap /mnt $desktop_packages >/dev/null
 }
 
-# Call desktop_selector function
-desktop_selector
-
 # User enters a password for the LUKS Container (function).
 lukspass_selector () {
     input_print "Please enter a password for the LUKS container (you're not going to see the password): "
@@ -217,296 +214,96 @@ microcode_detector () {
     if [[ "$CPU" == *"AuthenticAMD"* ]]; then
         info_print "An AMD CPU has been detected, the AMD microcode will be installed."
         microcode="amd-ucode"
-    else
+    elif [[ "$CPU" == *"GenuineIntel"* ]]; then
         info_print "An Intel CPU has been detected, the Intel microcode will be installed."
         microcode="intel-ucode"
     fi
 }
 
-# User enters a hostname (function).
-hostname_selector () {
-    input_print "Please enter the hostname: "
-    read -r hostname
-    if [[ -z "$hostname" ]]; then
-        error_print "You need to enter a hostname in order to continue."
-        return 1
-    fi
-    return 0
+# Partitioning and mounting (function)
+partition_setup () {
+    info_print "Setting up partitions..."
+    
+    # Example: Create a single LUKS encrypted partition for simplicity
+    lsblk
+    input_print "Enter the disk to partition (e.g., /dev/sda): "
+    read -r disk
+    
+    # Create partitions
+    parted "$disk" mklabel gpt
+    parted "$disk" mkpart primary fat32 1MiB 512MiB
+    parted "$disk" set 1 esp on
+    parted "$disk" mkpart primary ext4 512MiB 100%
+    
+    # Setup LUKS
+    echo -n "$password" | cryptsetup luksFormat "${disk}2" -
+    echo -n "$password" | cryptsetup open "${disk}2" cryptroot -
+    
+    # Format partitions
+    mkfs.fat -F32 "${disk}1"
+    mkfs.ext4 /dev/mapper/cryptroot
+    
+    # Mount partitions
+    mount /dev/mapper/cryptroot /mnt
+    mkdir /mnt/boot
+    mount "${disk}1" /mnt/boot
+    
+    info_print "Partitions have been set up and mounted."
 }
 
-# User chooses the locale (function).
-locale_selector () {
-    input_print "Please insert the locale you use (format: xx_XX. Enter empty to use en_US, or \"/\" to search locales): " locale
-    read -r locale
-    case "$locale" in
-        '') locale="en_US.UTF-8"
-            info_print "$locale will be the default locale."
-            return 0;;
-        '/') sed -E '/^# +|^#$/d;s/^#| *$//g;s/ .*/ (Charset:&)/' /etc/locale.gen | less -M
-                clear
-                return 1;;
-        *)  if ! grep -q "^#\?$(sed 's/[].*[]/\\&/g' <<< "$locale") " /etc/locale.gen; then
-                error_print "The specified locale doesn't exist or isn't supported."
-                return 1
-            fi
-            return 0
-    esac
-}
-
-# User chooses the console keyboard layout (function).
-keyboard_selector () {
-    input_print "Please insert the keyboard layout to use in console (enter empty to use US, or \"/\" to look up for keyboard layouts): "
-    read -r kblayout
-    case "$kblayout" in
-        '') kblayout="us"
-            info_print "The standard US keyboard layout will be used."
-            return 0;;
-        '/') localectl list-keymaps
-             clear
-             return 1;;
-        *) if ! localectl list-keymaps | grep -Fxq "$kblayout"; then
-               error_print "The specified keymap doesn't exist."
-               return 1
-           fi
-        info_print "Changing console layout to $kblayout."
-        loadkeys "$kblayout"
-        return 0
-    esac
-}
-
-# Welcome screen.
-echo -ne "${BOLD}${BYELLOW}
-
-░█████╗░██████╗░░█████╗░██╗░░██╗██████╗░██████╗░██╗███╗░░░███╗███████╗
-██╔══██╗██╔══██╗██╔══██╗██║░░██║██╔══██╗██╔══██╗██║████╗░████║██╔════╝
-███████║██████╔╝██║░░╚═╝███████║██████╔╝██████╔╝██║██╔████╔██║█████╗░░
-██╔══██║██╔══██╗██║░░██╗██╔══██║██╔═══╝░██╔══██╗██║██║╚██╔╝██║██╔══╝░░
-██║░░██║██║░░██║╚█████╔╝██║░░██║██║░░░░░██║░░██║██║██║░╚═╝░██║███████╗
-╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚══════╝
-${RESET}"
-info_print "Welcome to easy-arch, a script made in order to simplify the process of installing Arch Linux."
-
-# Setting up keyboard layout.
-until keyboard_selector; do : ; done
-
-# Choosing the target for the installation.
-info_print "Available disks for the installation:"
-PS3="Please select the number of the corresponding disk (e.g. 1): "
-select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
-do
-    DISK="$ENTRY"
-    info_print "Arch Linux will be installed on the following disk: $DISK"
-    break
-done
-
-# Setting up LUKS password.
-until lukspass_selector; do : ; done
-
-# Setting up the kernel.
-until kernel_selector; do : ; done
-
-# User choses the network.
-until network_selector; do : ; done
-
-# User choses the locale.
-until locale_selector; do : ; done
-
-# User choses the hostname.
-until hostname_selector; do : ; done
-
-# User sets up the user/root passwords.
-until userpass_selector; do : ; done
-until rootpass_selector; do : ; done
-
-# Warn user about deletion of old partition scheme.
-input_print "This will delete the current partition table on $DISK once installation starts. Do you agree [y/N]?: "
-read -r disk_response
-if ! [[ "${disk_response,,}" =~ ^(yes|y)$ ]]; then
-    error_print "Quitting."
-    exit
+# If you are not root, you cannot run this script.
+if [[ "$EUID" -ne 0 ]]; then
+    error_print "This script must be run as root, exiting."
+    exit 1
 fi
-info_print "Wiping $DISK."
-wipefs -af "$DISK" &>/dev/null
-sgdisk -Zo "$DISK" &>/dev/null
 
-# Creating a new partition scheme.
-info_print "Creating the partitions on $DISK."
-parted -s "$DISK" \
-    mklabel gpt \
-    mkpart ESP fat32 1MiB 513MiB \
-    set 1 esp on \
-    mkpart CRYPTROOT 513MiB 100% \
+info_print "Running pre-flight checks..."
 
-ESP="/dev/disk/by-partlabel/ESP"
-CRYPTROOT="/dev/disk/by-partlabel/CRYPTROOT"
-
-# Informing the Kernel of the changes.
-info_print "Informing the Kernel about the disk changes."
-partprobe "$DISK"
-
-# Formatting the ESP as FAT32.
-info_print "Formatting the EFI Partition as FAT32."
-mkfs.fat -F 32 "$ESP" &>/dev/null
-
-# Creating a LUKS Container for the root partition.
-info_print "Creating LUKS Container for the root partition."
-echo -n "$password" | cryptsetup luksFormat "$CRYPTROOT" -d - &>/dev/null
-echo -n "$password" | cryptsetup open "$CRYPTROOT" cryptroot -d - 
-BTRFS="/dev/mapper/cryptroot"
-
-# Formatting the LUKS Container as BTRFS.
-info_print "Formatting the LUKS container as BTRFS."
-mkfs.btrfs "$BTRFS" &>/dev/null
-mount "$BTRFS" /mnt
-
-# Creating BTRFS subvolumes.
-info_print "Creating BTRFS subvolumes."
-subvols=(snapshots var_pkgs var_log home root srv)
-for subvol in '' "${subvols[@]}"; do
-    btrfs su cr /mnt/@"$subvol" &>/dev/null
-done
-
-# Mounting the newly created subvolumes.
-umount /mnt
-info_print "Mounting the newly created subvolumes."
-mountopts="ssd,noatime,compress-force=zstd:3,discard=async"
-mount -o "$mountopts",subvol=@ "$BTRFS" /mnt
-mkdir -p /mnt/{home,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
-for subvol in "${subvols[@]:2}"; do
-    mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/"${subvol//_//}"
-done
-chmod 750 /mnt/root
-mount -o "$mountopts",subvol=@snapshots "$BTRFS" /mnt/.snapshots
-mount -o "$mountopts",subvol=@var_pkgs "$BTRFS" /mnt/var/cache/pacman/pkg
-chattr +C /mnt/var/log
-mount "$ESP" /mnt/boot/
-
-# Checking the microcode to install.
+# Microcode detection.
 microcode_detector
 
-# Pacstrap (setting up a base sytem onto the new root).
-info_print "Installing the base system (it may take a while)."
-pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo &>/dev/null
-
-# Setting up the hostname.
-echo "$hostname" > /mnt/etc/hostname
-
-# Generating /etc/fstab.
-info_print "Generating a new fstab."
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Configure selected locale and console keymap
-sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
-echo "LANG=$locale" > /mnt/etc/locale.conf
-echo "KEYMAP=$kblayout" > /mnt/etc/vconsole.conf
-
-# Setting hosts file.
-info_print "Setting hosts file."
-cat > /mnt/etc/hosts <<EOF
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $hostname.localdomain   $hostname
-EOF
-
-# Virtualization check.
+# Virtualization detection.
 virt_check
 
-# Setting up the network.
-network_installer
+info_print "The pre-flight checks are now complete."
 
-# Configuring /etc/mkinitcpio.conf.
-info_print "Configuring /etc/mkinitcpio.conf."
-cat > /mnt/etc/mkinitcpio.conf <<EOF
-HOOKS=(systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems)
-EOF
+# Printing the welcome message.
+info_print "Welcome to the guided installation of Arch Linux."
+info_print "Please select options by entering the corresponding number."
 
-# Setting up LUKS2 encryption in grub.
-info_print "Setting up grub config."
-UUID=$(blkid -s UUID -o value $CRYPTROOT)
-sed -i "\,^GRUB_CMDLINE_LINUX=\"\",s,\",&rd.luks.name=$UUID=cryptroot root=$BTRFS," /mnt/etc/default/grub
+# Partition setup
+partition_setup
 
-# Configuring the system.
-info_print "Configuring the system (timezone, system clock, initramfs, Snapper, GRUB)."
-arch-chroot /mnt /bin/bash -e <<EOF
-
-    # Setting up timezone.
-    ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime &>/dev/null
-
-    # Setting up clock.
-    hwclock --systohc
-
-    # Generating locales.
-    locale-gen &>/dev/null
-
-    # Generating a new initramfs.
-    mkinitcpio -P &>/dev/null
-
-    # Snapper configuration.
-    umount /.snapshots
-    rm -r /.snapshots
-    snapper --no-dbus -c root create-config /
-    btrfs subvolume delete /.snapshots &>/dev/null
-    mkdir /.snapshots
-    mount -a &>/dev/null
-    chmod 750 /.snapshots
-
-    # Installing GRUB.
-    grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB &>/dev/null
-
-    # Creating grub config file.
-    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
-
-EOF
-
-# Setting root password.
-info_print "Setting root password."
-echo "root:$rootpass" | arch-chroot /mnt chpasswd
-
-# Setting user password.
-if [[ -n "$username" ]]; then
-    echo "%wheel ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers.d/wheel
-    info_print "Adding the user $username to the system with root privilege."
-    arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$username"
-    info_print "Setting user password for $username."
-    echo "$username:$userpass" | arch-chroot /mnt chpasswd
-fi
-
-# Boot backup hook.
-info_print "Configuring /boot backup when pacman transactions are made."
-mkdir /mnt/etc/pacman.d/hooks
-cat > /mnt/etc/pacman.d/hooks/50-bootbackup.hook <<EOF
-[Trigger]
-Operation = Upgrade
-Operation = Install
-Operation = Remove
-Type = Path
-Target = usr/lib/modules/*/vmlinuz
-
-[Action]
-Depends = rsync
-Description = Backing up /boot...
-When = PostTransaction
-Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
-EOF
-
-# ZRAM configuration.
-info_print "Configuring ZRAM."
-cat > /mnt/etc/systemd/zram-generator.conf <<EOF
-[zram0]
-zram-size = min(ram, 8192)
-EOF
-
-# Pacman eye-candy features.
-info_print "Enabling colours, animations, and parallel downloads for pacman."
-sed -Ei 's/^#(Color)$/\1\nILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /mnt/etc/pacman.conf
-
-# Enabling various services.
-info_print "Enabling Reflector, automatic snapshots, BTRFS scrubbing and systemd-oomd."
-services=(reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd)
-for service in "${services[@]}"; do
-    systemctl enable "$service" --root=/mnt &>/dev/null
+while ! kernel_selector; do
+    continue
 done
 
-# Finishing up.
-info_print "Done, you may now wish to reboot (further changes can be done by chrooting into /mnt)."
-exit
+while ! network_selector; do
+    continue
+done
+
+while ! userpass_selector; do
+    continue
+done
+
+while ! rootpass_selector; do
+    continue
+done
+
+while ! lukspass_selector; do
+    continue
+done
+
+# Select and install desktop environment
+desktop_selector
+
+info_print "Installing kernel and bootloader."
+pacstrap /mnt base "$kernel" "$microcode" linux-firmware grub grub-btrfs efibootmgr sudo >/dev/null
+
+info_print "Generating an fstab."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+info_print "Setting up networking."
+network_installer
+
+info_print "Installation is now complete. Please reboot into your new system."
